6612d52772b824ad95014cb5ad619fa344acec5f
Darwash Web <darwashredes@gmail.com>
2026-06-30 15:50:39 +0000

    fix: handle missing avatar in ContactCard

--- cliente-darwash-web-remates-main/src/components/ContactCard.jsx
+++ cliente-darwash-web-remates-main/src/components/ContactCard.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 export default function ContactCard({ contact }) {
   const { avatar, title, email, phone } = contact.fields;
+  const avatarUrl = avatar?.fields?.file?.url;
   return (
     <div className="card mb-3">
       {/* eslint-disable-next-line */}
-      <img src={avatar.fields.file.url} alt="" className="card-img-top" />
+      {avatarUrl && <img src={avatarUrl} alt="" className="card-img-top" />}
       <div className="card-body">
         <h4>{title}</h4>
         <p className="card-text mb-0">{email}</p>
         <p className="card-text">{phone}</p>
         <a href={`https://wa.me/${phone}`} target="_blank" className="btn btn-success text-white" rel="noopener noreferrer">
-          <i class="fa-brands fa-whatsapp"></i> Contactar
+          <i className="fa-brands fa-whatsapp"></i> Contactar
         </a>
       </div>
     </div>
